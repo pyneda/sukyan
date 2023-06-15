406d0cbaab90119842d926f346041846f6422401
pyneda <11313340+pyneda@users.noreply.github.com>
2023-06-15 19:41:21 +0200

    add test for SetRequestHeadersFromHistoryItem

---
+++ pkg/http_utils/http_headers_test.go
@@ -0,0 +1,47 @@
+package http_utils
+
+import (
+	"net/http"
+	"testing"
+	"encoding/json"
+	"reflect"
+	"github.com/pyneda/sukyan/db"
+)
+
+
+
+func TestSetRequestHeadersFromHistoryItem(t *testing.T) {
+	request, _ := http.NewRequest("GET", "http://example.com", nil)
+	headers := RequestHeaders{
+		"Content-Type": []string{"application/json"},
+		"User-Agent":   []string{"test-agent"},
+	}
+	headersBytes, _ := json.Marshal(headers)
+
+	historyItem := &db.History{
+		RequestHeaders: headersBytes,
+	}
+
+	err := SetRequestHeadersFromHistoryItem(request, historyItem)
+	if err != nil {
+		t.Errorf("Expected no error, but got %v", err)
+	}
+
+	// Now we check if the headers were correctly set
+	for key, values := range headers {
+		for _, value := range values {
+			if !reflect.DeepEqual(request.Header.Values(key), []string{value}) {
+				t.Errorf("Expected header %s to be set to %s, but got %v", key, value, request.Header.Values(key))
+			}
+		}
+	}
+
+	// Test the error scenario
+	badHistoryItem := &db.History{
+		RequestHeaders: []byte("{"),
+	}
+	err = SetRequestHeadersFromHistoryItem(request, badHistoryItem)
+	if err == nil {
+		t.Errorf("Expected error due to bad JSON, but got none")
+	}
+}

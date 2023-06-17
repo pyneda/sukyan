55f0a5201b534b22a893f883ce30db4baff32064
pyneda <11313340+pyneda@users.noreply.github.com>
2023-06-17 11:13:26 +0200

    use the new details issue field in the browser audit issues

--- pkg/web/browser_audit_issues.go
+++ pkg/web/browser_audit_issues.go
@@ -2,7 +2,6 @@ package web
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/go-rod/rod/lib/proto"
 	"github.com/pyneda/sukyan/db"
 	"github.com/rs/zerolog/log"
@@ -18,56 +17,55 @@ func handleBrowserAuditIssues(url string, e *proto.AuditsIssueAdded) {
 	// Check if it is a Mixed Content Issue Details
 	// Codes in: https://github.com/go-rod/rod/blob/ba02d6c76c1e2ef7ab4a58909c58877b34761fd9/lib/proto/audits.go#L809
 	if e.Issue.Details.MixedContentIssueDetails != nil {
-		var description strings.Builder
-		description.WriteString("DETAILS:\nThe insecure content loaded url comes from: " + e.Issue.Details.MixedContentIssueDetails.InsecureURL)
+		var details strings.Builder
+		details.WriteString("The insecure content loaded url comes from: " + e.Issue.Details.MixedContentIssueDetails.InsecureURL)
 		if e.Issue.Details.MixedContentIssueDetails.Frame.FrameID != "" {
-			description.WriteString("\nAffected frame: " + string(e.Issue.Details.MixedContentIssueDetails.Frame.FrameID))
+			details.WriteString("\nAffected frame: " + string(e.Issue.Details.MixedContentIssueDetails.Frame.FrameID))
 		}
 		if e.Issue.Details.MixedContentIssueDetails.ResourceType != "" {
-			description.WriteString("\nResource type: " + string(e.Issue.Details.MixedContentIssueDetails.ResourceType))
+			details.WriteString("\nResource type: " + string(e.Issue.Details.MixedContentIssueDetails.ResourceType))
 		}
 		if e.Issue.Details.MixedContentIssueDetails.ResolutionStatus != "" {
-			description.WriteString("\nResolution status: " + string(e.Issue.Details.MixedContentIssueDetails.ResolutionStatus))
+			details.WriteString("\nResolution status: " + string(e.Issue.Details.MixedContentIssueDetails.ResolutionStatus))
 		}
 		if e.Issue.Details.MixedContentIssueDetails.MainResourceURL != "" {
-			description.WriteString("\nMain resource url: " + string(e.Issue.Details.MixedContentIssueDetails.MainResourceURL))
+			details.WriteString("\nMain resource url: " + string(e.Issue.Details.MixedContentIssueDetails.MainResourceURL))
 		}
 		browserAuditIssue := db.GetIssueTemplateByCode(db.MixedContentCode)
 		browserAuditIssue.URL = url
-		browserAuditIssue.Description = fmt.Sprintf("%v\n%v", browserAuditIssue.Description, description.String())
+		browserAuditIssue.Details = details.String()
 		browserAuditIssue.AdditionalInfo = jsonDetails
 		browserAuditIssue.Confidence = 80
 		db.Connection.CreateIssue(*browserAuditIssue)
 
 	} else if e.Issue.Details.CorsIssueDetails != nil {
-		var description strings.Builder
-		description.WriteString("DETAILS:\n")
+		var details strings.Builder
 		if e.Issue.Details.CorsIssueDetails.CorsErrorStatus != nil {
-			description.WriteString("\nCORS Error: " + string(e.Issue.Details.CorsIssueDetails.CorsErrorStatus.CorsError))
-			description.WriteString("\nCORS Error Failed Parameter: " + string(e.Issue.Details.CorsIssueDetails.CorsErrorStatus.FailedParameter))
+			details.WriteString("\nCORS Error: " + string(e.Issue.Details.CorsIssueDetails.CorsErrorStatus.CorsError))
+			details.WriteString("\nCORS Error Failed Parameter: " + string(e.Issue.Details.CorsIssueDetails.CorsErrorStatus.FailedParameter))
 
 		}
-		description.WriteString("\nIs Warning: " + strconv.FormatBool(e.Issue.Details.CorsIssueDetails.IsWarning))
+		details.WriteString("\nIs Warning: " + strconv.FormatBool(e.Issue.Details.CorsIssueDetails.IsWarning))
 		if e.Issue.Details.CorsIssueDetails.Location != nil {
-			description.WriteString("\nSource code location:")
-			description.WriteString("\n		- URL: " + string(e.Issue.Details.CorsIssueDetails.Location.URL))
-			description.WriteString("\n		- Line number: " + strconv.Itoa(e.Issue.Details.CorsIssueDetails.Location.LineNumber))
-			description.WriteString("\n		- Column number: " + strconv.Itoa(e.Issue.Details.CorsIssueDetails.Location.ColumnNumber))
+			details.WriteString("\nSource code location:")
+			details.WriteString("\n		- URL: " + string(e.Issue.Details.CorsIssueDetails.Location.URL))
+			details.WriteString("\n		- Line number: " + strconv.Itoa(e.Issue.Details.CorsIssueDetails.Location.LineNumber))
+			details.WriteString("\n		- Column number: " + strconv.Itoa(e.Issue.Details.CorsIssueDetails.Location.ColumnNumber))
 
 		}
 		if e.Issue.Details.CorsIssueDetails.InitiatorOrigin != "" {
-			description.WriteString("\nInitiator Origin: " + string(e.Issue.Details.CorsIssueDetails.InitiatorOrigin))
+			details.WriteString("\nInitiator Origin: " + string(e.Issue.Details.CorsIssueDetails.InitiatorOrigin))
 		}
 		if e.Issue.Details.CorsIssueDetails.ClientSecurityState != nil {
-			description.WriteString("\nNetwork Client Security State:")
-			description.WriteString("\n		- Initiator is secure context: " + strconv.FormatBool(e.Issue.Details.CorsIssueDetails.ClientSecurityState.InitiatorIsSecureContext))
-			description.WriteString("\n		- Initiator IP address space: " + string(e.Issue.Details.CorsIssueDetails.ClientSecurityState.InitiatorIPAddressSpace))
-			description.WriteString("\n		- Private network request policy: " + string(e.Issue.Details.CorsIssueDetails.ClientSecurityState.PrivateNetworkRequestPolicy))
+			details.WriteString("\nNetwork Client Security State:")
+			details.WriteString("\n		- Initiator is secure context: " + strconv.FormatBool(e.Issue.Details.CorsIssueDetails.ClientSecurityState.InitiatorIsSecureContext))
+			details.WriteString("\n		- Initiator IP address space: " + string(e.Issue.Details.CorsIssueDetails.ClientSecurityState.InitiatorIPAddressSpace))
+			details.WriteString("\n		- Private network request policy: " + string(e.Issue.Details.CorsIssueDetails.ClientSecurityState.PrivateNetworkRequestPolicy))
 
 		}
 		browserAuditIssue := db.GetIssueTemplateByCode(db.CorsCode)
 		browserAuditIssue.URL = url
-		browserAuditIssue.Description = fmt.Sprintf("%v\n%v", browserAuditIssue.Description, description.String())
+		browserAuditIssue.Details = details.String()
 		browserAuditIssue.AdditionalInfo = jsonDetails
 		browserAuditIssue.Confidence = 80
 		db.Connection.CreateIssue(*browserAuditIssue)

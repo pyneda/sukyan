df08dee6316723db56bd2e9f996648a5c8e07e15
pyneda <11313340+pyneda@users.noreply.github.com>
2023-06-17 10:55:01 +0200

    passive checks for private ip and email addresses and file uploads

--- pkg/passive/scan.go
+++ pkg/passive/scan.go
@@ -4,6 +4,8 @@ import (
 	"github.com/pyneda/sukyan/db"
 	"github.com/rs/zerolog/log"
 	"strings"
+	"regexp"
+	"fmt"
 
 	wappalyzer "github.com/projectdiscovery/wappalyzergo"
 )
@@ -19,6 +21,9 @@ func ScanHistoryItem(item *db.History) {
 	} else if strings.Contains(item.ContentType, "javascript") {
 		PassiveJavascriptScan(item)
 	}
+	PrivateIPScan(item)
+	EmailAddressScan(item)
+	FileUploadScan(item)
 
 }
 
@@ -48,6 +53,65 @@ func DirectoryListingScan(item *db.History) {
 		}
 	}
 	if isDirectoryListing {
-		db.CreateIssueFromHistoryAndTemplate(item, db.DirectoryListingCode, 90)
+		db.CreateIssueFromHistoryAndTemplate(item, db.DirectoryListingCode, "", 90)
+	}
+}
+
+
+var privateIPRegex = regexp.MustCompile(`\b((10\.\d{1,3}\.\d{1,3}\.\d{1,3})|(172\.(1[6-9]|2\d|3[0-1])\.\d{1,3}\.\d{1,3})|(192\.168\.\d{1,3}\.\d{1,3})|(127\.0\.0\.1))\b`)
+
+func PrivateIPScan(item *db.History) {
+	matchAgainst := item.RawResponse
+	if matchAgainst == "" {
+		matchAgainst = item.ResponseBody
+	}
+	matches := privateIPRegex.FindAllString(matchAgainst, -1)
+	
+	if len(matches) > 0 {
+		var sb strings.Builder
+		sb.WriteString("Discovered Internal IP addresses:")
+		for _, match := range matches {
+			sb.WriteString(fmt.Sprintf("\n - %s", match))
+		}
+		discoveredIPs := sb.String()
+		db.CreateIssueFromHistoryAndTemplate(item, db.PrivateIPsCode, discoveredIPs, 90)
+	}
+}
+
+
+var emailRegex = regexp.MustCompile(`\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b`)
+
+func EmailAddressScan(item *db.History) {
+	matchAgainst := item.RawResponse
+	if matchAgainst == "" {
+		matchAgainst = item.ResponseBody
+	}
+	matches := emailRegex.FindAllString(matchAgainst, -1)
+
+	if len(matches) > 0 {
+		var sb strings.Builder
+		sb.WriteString("Discovered email addresses:")
+		for _, match := range matches {
+			sb.WriteString(fmt.Sprintf("\n - %s", match))
+		}
+		discoveredEmails := sb.String()
+		db.CreateIssueFromHistoryAndTemplate(item, db.EmailAddressesCode, discoveredEmails, 90)
+	}
+}
+
+
+var fileUploadRegex = regexp.MustCompile(`(?i)<input[^>]*type=["']?file["']?`)
+
+func FileUploadScan(item *db.History) {
+	// This is too simple, could also check the headers for content-type: multipart/form-data and other things
+	matches := emailRegex.FindAllString(matchAgainst, -1)
+	if len(matches) > 0 {
+		var sb strings.Builder
+		sb.WriteString("Discovered file upload inputs:")
+		for _, match := range matches {
+			sb.WriteString(fmt.Sprintf("\n - %s", match))
+		}
+		details := sb.String()
+		db.CreateIssueFromHistoryAndTemplate(item, db.FileUploadDetectedCode, details, 90)
 	}
 }

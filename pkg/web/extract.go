604c8705e4d45a62bbb4a60e8a45acc9517b15f6
pyneda <11313340+pyneda@users.noreply.github.com>
2023-07-04 03:17:19 +0200

    move browser related code from web pkg to a specific browser package, deprecate old code, handle browser and page pool manager and minor crawler improvements

---
+++ pkg/web/extract.go
@@ -0,0 +1,28 @@
+package web
+
+import (
+	"fmt"
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/proto"
+)
+
+// GetPageAnchors find anchors on the given page
+func GetPageAnchors(p *rod.Page) (anchors []string, err error) {
+	anchors = []string{}
+	resp := p.MustEval(GetLinks)
+	for _, link := range resp.Arr() {
+		anchors = append(anchors, link.String())
+	}
+	// log.Info().Strs("anchors", anchors).Int("count", len(anchors)).Msg("Page anchors gathered")
+	return anchors, nil
+}
+
+// GetPageResources gets all the page loaded resources, not used by now and probably can be removed as all requests are already hijacked
+func GetPageResources(p *rod.Page) ([]byte, error) {
+	response, err := proto.PageGetResourceTree{}.Call(p)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(response.FrameTree)
+	return nil, nil
+}

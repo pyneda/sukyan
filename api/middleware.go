05391e2a4e31a876648b272f1d0d2bd5c7126385
pyneda <11313340+pyneda@users.noreply.github.com>
2023-08-05 23:44:57 +0200

    fix refresh token endpoint and other auth related improvements

--- api/middleware.go
+++ api/middleware.go
@@ -3,15 +3,16 @@ package api
 import (
 	jwtMiddleware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
-	"os"
+	"github.com/spf13/viper"
 )
 
 // JWTProtected func for specify routes group with JWT authentication.
 // See: https://github.com/gofiber/contrib/jwt
 func JWTProtected() func(*fiber.Ctx) error {
 	// Create config for JWT authentication middleware.
+	jwtSecret := viper.GetString("api.auth.jwt_secret_key")
 	config := jwtMiddleware.Config{
-		SigningKey:   jwtMiddleware.SigningKey{Key: []byte(os.Getenv("JWT_SECRET_KEY"))},
+		SigningKey:   jwtMiddleware.SigningKey{Key: []byte(jwtSecret)},
 		ContextKey:   "jwt", // used in private routes
 		ErrorHandler: jwtError,
 	}

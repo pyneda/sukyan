0c47a215d0770e14e1d80abb2fc35a77196c4a45
pyneda <11313340+pyneda@users.noreply.github.com>
2023-08-10 05:57:55 +0200

    lib packages

---
+++ lib/browser/browser_pool.go
@@ -0,0 +1,86 @@
+package browser
+
+import (
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/launcher"
+	// "github.com/go-rod/rod/lib/proto"
+	"github.com/spf13/viper"
+)
+
+type BrowserPoolManagerConfig struct {
+	PoolSize int
+	Source   string
+}
+
+type BrowserPoolManager struct {
+	launcher             *launcher.Launcher
+	browser              *rod.Browser
+	pool                 rod.BrowserPool
+	config               BrowserPoolManagerConfig
+	HijackResultsChannel chan HijackResult
+	hijack               bool
+	workspaceID          uint
+}
+
+func NewBrowserPoolManager(config BrowserPoolManagerConfig, workspaceID uint) *BrowserPoolManager {
+	manager := BrowserPoolManager{
+		config:      config,
+		workspaceID: workspaceID,
+	}
+	manager.Start()
+
+	return &manager
+}
+
+func NewHijackedBrowserPoolManager(config BrowserPoolManagerConfig, hijackResultsChannel chan HijackResult, workspaceID uint) *BrowserPoolManager {
+	manager := BrowserPoolManager{
+		config:               config,
+		HijackResultsChannel: hijackResultsChannel,
+		hijack:               true,
+		workspaceID:          workspaceID,
+	}
+	manager.Start()
+
+	return &manager
+}
+
+func (b *BrowserPoolManager) Start() {
+	poolSize := 4
+	if b.config.PoolSize > 0 {
+		poolSize = b.config.PoolSize
+	}
+
+	b.pool = rod.NewBrowserPool(poolSize)
+}
+
+func (b *BrowserPoolManager) NewBrowser() *rod.Browser {
+	browser := b.pool.Get(b.createBrowser)
+
+	// if b.config.UserAgent != "" {
+	// 	_ = browser.SetUserAgent(&proto.NetworkSetUserAgentOverride{UserAgent: "Test"})
+	// } else if viper.GetString("navigation.user_agent") != "" {
+	// 	_ = browser.SetUserAgent(&proto.NetworkSetUserAgentOverride{UserAgent: viper.GetString("navigation.user_agent")})
+	// }
+
+	return browser
+}
+
+func (b *BrowserPoolManager) ReleaseBrowser(browser *rod.Browser) {
+	b.pool.Put(browser)
+}
+
+func (b *BrowserPoolManager) createBrowser() *rod.Browser {
+	l := GetBrowserLauncher()
+	controlURL := l.MustLaunch()
+	browser := rod.New().ControlURL(controlURL).MustConnect()
+	go browser.HandleAuth(viper.GetString("navigation.auth.basic.username"), viper.GetString("navigation.auth.basic.password"))()
+	if b.hijack {
+		Hijack(HijackConfig{AnalyzeJs: true, AnalyzeHTML: true}, browser, b.config.Source, b.HijackResultsChannel, b.workspaceID)
+	}
+	return browser
+}
+
+func (b *BrowserPoolManager) Close() {
+	b.pool.Cleanup(func(p *rod.Browser) { p.MustClose() })
+	b.browser.Close()
+}
---
+++ lib/browser/hijack.go
@@ -0,0 +1,158 @@
+package browser
+
+import (
+	"encoding/json"
+	"github.com/pyneda/sukyan/db"
+	"github.com/pyneda/sukyan/lib"
+	"github.com/pyneda/sukyan/pkg/passive"
+	"net/http"
+	"strings"
+
+	"fmt"
+	"github.com/go-rod/rod"
+	"github.com/rs/zerolog/log"
+	"gorm.io/datatypes"
+)
+
+// HijackConfig represents a hijack configuration to apply when using the browser
+type HijackConfig struct {
+	AnalyzeJs   bool
+	AnalyzeHTML bool
+}
+
+type HijackResult struct {
+	History        *db.History
+	DiscoveredURLs []string
+}
+
+func Hijack(config HijackConfig, browser *rod.Browser, source string, resultsChannel chan HijackResult, workspaceID uint) {
+	router := browser.HijackRequests()
+	ignoreKeywords := []string{"google", "pinterest", "facebook", "instagram", "127.0.0.2"}
+
+	router.MustAdd("*", func(ctx *rod.Hijack) {
+
+		// ctx.MustLoadResponse()
+		err := ctx.LoadResponse(http.DefaultClient, true)
+
+		if err != nil {
+			log.Error().Err(err).Str("url", ctx.Request.URL().String()).Msg("Error loading hijacked response")
+		}
+
+		// contentType := ctx.Response.Headers().Get("Content-Type")
+		mustSkip := false
+		for _, skipWord := range ignoreKeywords {
+			if strings.Contains(ctx.Request.URL().Host, skipWord) == true {
+				mustSkip = true
+			}
+		}
+		if mustSkip {
+			log.Debug().Str("url", ctx.Request.URL().String()).Msg("Skipping processing of hijacked response")
+		} else {
+			go func() {
+				history := CreateHistoryFromHijack(ctx.Request, ctx.Response, source, "Create history from hijack", workspaceID)
+				// passive.ScanHistoryItem(history)
+				linksFound := passive.ExtractedURLS{}
+				if ctx.Request.Type() != "Image" && ctx.Request.Type() != "Font" && ctx.Request.Type() != "Media" {
+					// linksFound = passive.ExtractAndAnalyzeURLS(string(history.RawResponse), history.URL)
+					linksFound = passive.ExtractURLsFromHistoryItem(history)
+
+				}
+				hijackResult := HijackResult{
+					History:        history,
+					DiscoveredURLs: linksFound.Web,
+				}
+				if resultsChannel != nil {
+					resultsChannel <- hijackResult
+				}
+			}()
+		}
+
+	})
+	go router.Run()
+}
+
+func DumpHijackRequest(req *rod.HijackRequest) string {
+	var dump strings.Builder
+
+	// Request Line
+	dump.WriteString(fmt.Sprintf("%s %s %s\n", req.Method(), req.URL(), "HTTP/1.1")) // Using HTTP/1.1 as a placeholder
+
+	// Headers
+	for k, v := range req.Headers() {
+		dump.WriteString(fmt.Sprintf("%s: %s\n", k, v))
+	}
+
+	// Body
+	body := req.Body()
+	if len(body) > 0 {
+		dump.WriteString("\n")
+		dump.WriteString(string(body))
+	}
+
+	return dump.String()
+}
+
+func DumpHijackResponse(res *rod.HijackResponse) string {
+	var dump strings.Builder
+
+	// Status Line
+	dump.WriteString(fmt.Sprintf("HTTP/1.1 %d %s\n", res.Payload().ResponseCode, http.StatusText(res.Payload().ResponseCode))) // Using HTTP/1.1 as a placeholder
+
+	// Headers
+	for k, values := range res.Headers() {
+		for _, v := range values {
+			dump.WriteString(fmt.Sprintf("%s: %s\n", k, v))
+		}
+	}
+
+	// Body
+	body := res.Body()
+	if len(body) > 0 {
+		dump.WriteString("\n")
+		dump.WriteString(string(body))
+	}
+
+	return dump.String()
+}
+
+// CreateHistoryFromHijack saves a history request from hijack request/response items.
+func CreateHistoryFromHijack(request *rod.HijackRequest, response *rod.HijackResponse, source string, note string, workspaceID uint) *db.History {
+	requestHeaders, err := json.Marshal(request.Headers())
+	if err != nil {
+		log.Error().Err(err).Msg("Error converting request headers to json")
+	}
+	responseHeaders, err := json.Marshal(response.Headers())
+	if err != nil {
+		log.Error().Err(err).Msg("Error converting response headers to json")
+	}
+	rawRequest := DumpHijackRequest(request)
+	rawResponse := DumpHijackResponse(response)
+	historyUrl := request.URL().String()
+	reqBody := request.Body()
+	history := db.History{
+		StatusCode:           response.Payload().ResponseCode,
+		URL:                  historyUrl,
+		Depth:                lib.CalculateURLDepth(historyUrl),
+		RequestHeaders:       datatypes.JSON(requestHeaders),
+		RequestBody:          []byte(reqBody),
+		RequestBodySize:      len(reqBody),
+		RequestContentLength: request.Req().ContentLength,
+		RequestContentType:   request.Req().Header.Get("Content-Type"),
+		ResponseHeaders:      datatypes.JSON(responseHeaders),
+		ResponseBody:         []byte(response.Body()),
+		ResponseContentType:  response.Headers().Get("Content-Type"),
+		Evaluated:            false,
+		Method:               request.Method(),
+		// ParametersCount:      len(request.URL().Query()),
+		Note:        note,
+		Source:      source,
+		RawRequest:  []byte(rawRequest),
+		RawResponse: []byte(rawResponse),
+		// ResponseContentLength: response.ContentLength,
+		WorkspaceID: &workspaceID,
+	}
+	createdHistory, _ := db.Connection.CreateHistory(&history)
+	log.Debug().Interface("history", history).Msg("New history record created")
+
+	return createdHistory
+}

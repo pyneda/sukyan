59f7df1a796996ba50683220b66ad5aa8ccc2b33
pyneda <11313340+pyneda@users.noreply.github.com>
2023-08-10 05:43:46 +0200

    update test

--- api/history_test.go
+++ api/history_test.go
@@ -7,12 +7,15 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert"
+	"github.com/pyneda/sukyan/db"
 )
 
 func TestFindHistory(t *testing.T) {
+	db.Connection.CreateDefaultWorkspace()
 	app := fiber.New()
 
 	app.Get("/history", FindHistory)
+
 
 	req := httptest.NewRequest("GET", "/history?page=1&page_size=10&status=200,404&workspace=1&methods=GET,POST&sources=scan", nil)
 	resp, _ := app.Test(req)

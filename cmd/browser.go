55bb865022922882c153e4e0a9c4e54043c73cfd
pyneda <11313340+pyneda@users.noreply.github.com>
2023-08-24 02:56:40 +0200

    don't create default workspace

--- cmd/browser.go
+++ cmd/browser.go
@@ -21,8 +21,7 @@ var browserCmd = &cobra.Command{
 			log.Error().Uint("id", workspaceID).Msg("Workspace does not exist")
 			workspaces, count, _ := db.Connection.ListWorkspaces(db.WorkspaceFilters{})
 			if count == 0 {
-				log.Info().Msg("No workspaces found, creating default")
-				db.Connection.CreateDefaultWorkspace()
+				log.Info().Msg("No workspaces found.")
 			} else {
 				log.Info().Msg("Available workspaces:")
 				for _, workspace := range workspaces {

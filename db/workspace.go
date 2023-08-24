ef8c77f14a9689b9c6de5530e749c94da32fe7ff
pyneda <11313340+pyneda@users.noreply.github.com>
2023-08-24 03:47:14 +0200

    add new workspace methods and improve current ones and add tests for all existing methods

--- db/workspace.go
+++ db/workspace.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"github.com/rs/zerolog/log"
+	"gorm.io/gorm"
 )
 
 // Workspace is used to group projects
@@ -58,18 +59,6 @@ func (d *DatabaseConnection) ListWorkspaces(filters WorkspaceFilters) (items []*
 	if result.Error != nil {
 		err = result.Error
 	}
-
-	if count == 0 {
-		log.Info().Msg("No workspaces found.")
-
-		workspace, err := d.CreateDefaultWorkspace()
-		if err != nil {
-			log.Error().Err(err).Msg("Error creating default workspace")
-		} else {
-			items = append(items, workspace)
-			count = 1
-		}
-	}
 	return items, count, err
 }
 
@@ -79,7 +68,7 @@ func (d *DatabaseConnection) CreateDefaultWorkspace() (*Workspace, error) {
 		Title:       "Default workspace",
 		Description: "Default workspace",
 	}
-	return d.CreateWorkspace(&workspace)
+	return d.GetOrCreateWorkspace(&workspace)
 }
 
 // CreateWorkspace saves a workspace to the database
@@ -93,9 +82,60 @@ func (d *DatabaseConnection) CreateWorkspace(workspace *Workspace) (*Workspace, 
 
 // GetOrCreateWorkspace gets a workspace with the given code, or creates it if it doesn't exist
 func (d *DatabaseConnection) GetOrCreateWorkspace(workspace *Workspace) (*Workspace, error) {
-	result := d.db.FirstOrCreate(workspace, Workspace{Code: workspace.Code})
-	if result.Error != nil {
-		log.Error().Err(result.Error).Interface("workspace", workspace).Msg("Workspace get or create failed")
+
+	var existingWorkspace Workspace
+	if err := d.db.Where("code = ?", workspace.Code).First(&existingWorkspace).Error; err == nil {
+		return &existingWorkspace, nil
+	} else if err != gorm.ErrRecordNotFound {
+		log.Error().Err(err).Interface("workspace", workspace).Msg("Error checking workspace by code")
+		return nil, err
 	}
-	return workspace, result.Error
+
+	if err := d.db.Create(&workspace).Error; err != nil {
+		log.Error().Err(err).Interface("workspace", workspace).Msg("Workspace creation failed")
+		return nil, err
+	}
+
+	return workspace, nil
+}
+
+// DeleteWorkspace deletes a workspace by ID
+func (d *DatabaseConnection) DeleteWorkspace(id uint) error {
+	var workspace Workspace
+	if err := d.db.Where("id = ?", id).Delete(&workspace).Error; err != nil {
+		log.Error().Err(err).Interface("id", id).Msg("Unable to delete workspace by ID")
+		return err
+	}
+	return nil
+}
+
+// UpdateWorkspace updates a workspace by its ID with the provided fields
+func (d *DatabaseConnection) UpdateWorkspace(id uint, updatedWorkspace *Workspace) error {
+	var workspace Workspace
+
+	// Fetch the workspace by ID
+	if err := d.db.Where("id = ?", id).First(&workspace).Error; err != nil {
+		log.Error().Err(err).Interface("id", id).Msg("Unable to fetch workspace by ID for updating")
+		return err
+	}
+
+	// Update the relevant fields
+	if updatedWorkspace.Code != "" {
+		workspace.Code = updatedWorkspace.Code
+	}
+
+	if updatedWorkspace.Title != "" {
+		workspace.Title = updatedWorkspace.Title
+	}
+
+	if updatedWorkspace.Description != "" {
+		workspace.Description = updatedWorkspace.Description
+	}
+
+	// Save the updated workspace
+	if err := d.db.Save(&workspace).Error; err != nil {
+		log.Error().Err(err).Interface("workspace", workspace).Msg("Unable to update workspace")
+		return err
+	}
+	return nil
 }

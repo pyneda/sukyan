6c79f0284fe99c62a39e4951c578f2a3c6d46543
pyneda <11313340+pyneda@users.noreply.github.com>
2023-09-07 02:01:54 +0200

    update task jobs list endpoint to allow more filters and sort parameters

--- db/task_job.go
+++ db/task_job.go
@@ -27,42 +27,103 @@ type TaskJob struct {
 }
 
 type TaskJobFilter struct {
-	Statuses    []string
-	Titles      []string
-	CompletedAt *time.Time
-	Pagination  Pagination
-	TaskID      uint
+	Statuses    []string   `json:"statuses" validate:"omitempty,dive,oneof=scheduled running finished failed"`
+	Titles      []string   `json:"titles" validate:"omitempty,dive,ascii"`
+	CompletedAt *time.Time `json:"completed_at" validate:"omitempty"`
+	Pagination  Pagination `json:"pagination"`
+	TaskID      uint       `json:"task_id" validate:"omitempty,numeric"`
+	StatusCodes []int      `json:"status_codes" validate:"omitempty,dive,numeric"`
+	Methods     []string   `json:"methods" validate:"omitempty,dive,oneof=GET POST PUT DELETE PATCH HEAD OPTIONS TRACE"`
+	SortBy      string     `json:"sort_by" validate:"omitempty,oneof=id history_method history_url history_status history_parameters_count title status started_at completed_at created_at updated_at"`
+	SortOrder   string     `json:"sort_order" validate:"omitempty,oneof=asc desc"`
+}
+
+var TaskJobSortFieldMap = map[string]string{
+	"id":                       "id",
+	"history_method":           "histories.method",
+	"history_url":              "histories.url",
+	"history_status":           "histories.status_code",
+	"history_parameters_count": "histories.parameters_count",
+	"title":                    "title",
+	"status":                   "status",
+	"started_at":               "started_at",
+	"completed_at":             "completed_at",
+	"created_at":               "created_at",
+	"updated_at":               "updated_at",
 }
 
 func (d *DatabaseConnection) ListTaskJobs(filter TaskJobFilter) (items []*TaskJob, count int64, err error) {
-	filterQuery := make(map[string]interface{})
-
-	if len(filter.Statuses) > 0 {
-		filterQuery["status"] = filter.Statuses
-	}
-
-	if len(filter.Titles) > 0 {
-		filterQuery["title"] = filter.Titles
-	}
-
-	if filter.TaskID != 0 {
-		filterQuery["task_id"] = filter.TaskID
-	}
-
 	query := d.db.Preload("History") // Eager load History
 
-	if filterQuery != nil && len(filterQuery) > 0 {
-		err = query.Scopes(Paginate(&filter.Pagination)).Where(filterQuery).Order("created_at desc").Find(&items).Error
-		d.db.Model(&TaskJob{}).Where(filterQuery).Count(&count)
-	} else {
-		err = query.Scopes(Paginate(&filter.Pagination)).Order("created_at desc").Find(&items).Error
-		d.db.Model(&TaskJob{}).Count(&count)
+	// Basic filters
+	if len(filter.Statuses) > 0 {
+		query = query.Where("status IN ?", filter.Statuses)
 	}
+	if len(filter.Titles) > 0 {
+		query = query.Where("title IN ?", filter.Titles)
+	}
+	if filter.TaskID != 0 {
+		query = query.Where("task_id = ?", filter.TaskID)
+	}
+
+	// Filters related to History
+	if len(filter.StatusCodes) > 0 {
+		query = query.Joins("JOIN histories ON histories.id = task_jobs.history_id").Where("histories.status_code IN ?", filter.StatusCodes)
+	}
+	if len(filter.Methods) > 0 {
+		query = query.Joins("JOIN histories ON histories.id = task_jobs.history_id").Where("histories.method IN ?", filter.Methods)
+	}
+
+	// Sorting
+
+	if sortField, exists := TaskJobSortFieldMap[filter.SortBy]; exists {
+		sortOrder := "asc"
+		if filter.SortOrder == "desc" {
+			sortOrder = "desc"
+		}
+		query = query.Order(sortField + " " + sortOrder)
+	} else {
+		query = query.Order("id desc")
+	}
+
+	// Pagination and final query
+	err = query.Scopes(Paginate(&filter.Pagination)).Find(&items).Error
+	d.db.Model(&TaskJob{}).Count(&count)
 
 	log.Info().Interface("filters", filter).Int("gathered", len(items)).Int("count", int(count)).Int("total_results", len(items)).Msg("Getting task job items")
 
 	return items, count, err
 }
+
+// func (d *DatabaseConnection) ListTaskJobs(filter TaskJobFilter) (items []*TaskJob, count int64, err error) {
+// 	filterQuery := make(map[string]interface{})
+
+// 	if len(filter.Statuses) > 0 {
+// 		filterQuery["status"] = filter.Statuses
+// 	}
+
+// 	if len(filter.Titles) > 0 {
+// 		filterQuery["title"] = filter.Titles
+// 	}
+
+// 	if filter.TaskID != 0 {
+// 		filterQuery["task_id"] = filter.TaskID
+// 	}
+
+// 	query := d.db.Preload("History") // Eager load History
+
+// 	if filterQuery != nil && len(filterQuery) > 0 {
+// 		err = query.Scopes(Paginate(&filter.Pagination)).Where(filterQuery).Order("created_at desc").Find(&items).Error
+// 		d.db.Model(&TaskJob{}).Where(filterQuery).Count(&count)
+// 	} else {
+// 		err = query.Scopes(Paginate(&filter.Pagination)).Order("created_at desc").Find(&items).Error
+// 		d.db.Model(&TaskJob{}).Count(&count)
+// 	}
+
+// 	log.Info().Interface("filters", filter).Int("gathered", len(items)).Int("count", int(count)).Int("total_results", len(items)).Msg("Getting task job items")
+
+// 	return items, count, err
+// }
 
 func (d *DatabaseConnection) NewTaskJob(taskID uint, title string, status TaskJobStatus, historyID uint) (*TaskJob, error) {
 	task := &TaskJob{

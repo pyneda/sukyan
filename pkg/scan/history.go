dc0999d47ae52898ba7883a5ebc61b64bde4da28
pyneda <11313340+pyneda@users.noreply.github.com>
2023-09-16 17:02:19 +0200

    add scan options parameter to ActiveScanHistoryItem and launch the params and headers tests based on them instead of viper config

--- pkg/scan/history.go
+++ pkg/scan/history.go
@@ -13,15 +13,15 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func ActiveScanHistoryItem(item *db.History, interactionsManager *integrations.InteractionsManager, payloadGenerators []*generation.PayloadGenerator, workspaceID uint) {
-	taskLog := log.With().Uint("workspace", workspaceID).Str("item", item.URL).Str("method", item.Method).Int("ID", int(item.ID)).Logger()
+func ActiveScanHistoryItem(item *db.History, interactionsManager *integrations.InteractionsManager, payloadGenerators []*generation.PayloadGenerator, options HistoryItemScanOptions) {
+	taskLog := log.With().Uint("workspace", options.WorkspaceID).Str("item", item.URL).Str("method", item.Method).Int("ID", int(item.ID)).Logger()
 	taskLog.Info().Msg("Starting to scan history item")
 
 	fuzzer := fuzz.HttpFuzzer{
 		Concurrency:         10,
 		InteractionsManager: interactionsManager,
 		AvoidRepeatedIssues: viper.GetBool("scan.avoid_repeated_issues"),
-		WorkspaceID:         workspaceID,
+		WorkspaceID:         options.WorkspaceID,
 	}
 	insertionPoints, _ := fuzz.GetInsertionPoints(item)
 	taskLog.Debug().Interface("insertionPoints", insertionPoints).Msg("Insertion points")
@@ -29,21 +29,21 @@ func ActiveScanHistoryItem(item *db.History, interactionsManager *integrations.I
 
 	cspp := active.ClientSidePrototypePollutionAudit{
 		HistoryItem: item,
-		WorkspaceID: workspaceID,
+		WorkspaceID: options.WorkspaceID,
 	}
 	cspp.Run()
 
 	var specificParamsToTest []string
 	p := web.WebPage{URL: item.URL}
 	hasParams, _ := p.HasParameters()
-	if hasParams && viper.GetBool("scan.insertion_points.parameters") {
+	if hasParams && options.IsScopedInsertionPoint("param") {
 		// ssrf := active.SSRFAudit{
 		// 	URL:                 item.URL,
 		// 	ParamsToTest:        specificParamsToTest,
 		// 	Concurrency:         5,
 		// 	StopAfterSuccess:    false,
 		// 	InteractionsManager: interactionsManager,
-		// 	WorkspaceID:         workspaceID,
+		// 	WorkspaceID:         options.WorkspaceID,
 		// }
 		// ssrf.Run()
 		active.TestXSS(item.URL, specificParamsToTest, "default.txt", false)
@@ -68,28 +68,28 @@ func ActiveScanHistoryItem(item *db.History, interactionsManager *integrations.I
 	// sni := active.SNIAudit{
 	// 	HistoryItem:         item,
 	// 	InteractionsManager: interactionsManager,
-	// 	WorkspaceID:         workspaceID,
+	// 	WorkspaceID:         options.WorkspaceID,
 	// }
 	// sni.Run()
 	methods := active.HTTPMethodsAudit{
 		HistoryItem: item,
 		Concurrency: 5,
-		WorkspaceID: workspaceID,
+		WorkspaceID: options.WorkspaceID,
 	}
 	methods.Run()
 
-	if viper.GetBool("scan.insertion_points.headers") {
+	if options.IsScopedInsertionPoint("headers") {
 		log4shell := active.Log4ShellInjectionAudit{
 			URL:                 item.URL,
 			Concurrency:         10,
 			InteractionsManager: interactionsManager,
-			WorkspaceID:         workspaceID,
+			WorkspaceID:         options.WorkspaceID,
 		}
 		log4shell.Run()
 		hostHeader := active.HostHeaderInjectionAudit{
 			URL:         item.URL,
 			Concurrency: 10,
-			WorkspaceID: workspaceID,
+			WorkspaceID: options.WorkspaceID,
 		}
 		hostHeader.Run()
 	}

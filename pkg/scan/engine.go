90abdbdac6847b0332310a34f02d2a951a888540
pyneda <11313340+pyneda@users.noreply.github.com>
2023-09-16 17:02:19 +0200

    add scan options parameter to ActiveScanHistoryItem and launch the params and headers tests based on them instead of viper config

--- pkg/scan/engine.go
+++ pkg/scan/engine.go
@@ -136,47 +136,10 @@ func (s *ScanEngine) schedulePassiveScan(item *db.History, workspaceID uint) {
 func (s *ScanEngine) scheduleActiveScan(item *db.History, taskJob *db.TaskJob, options HistoryItemScanOptions) {
 	taskJob.Status = db.TaskJobRunning
 	db.Connection.UpdateTaskJob(taskJob)
-	ActiveScanHistoryItem(item, s.InteractionsManager, s.payloadGenerators, options.WorkspaceID)
+	ActiveScanHistoryItem(item, s.InteractionsManager, s.payloadGenerators, options)
 	taskJob.Status = db.TaskJobFinished
 	taskJob.CompletedAt = time.Now()
 	db.Connection.UpdateTaskJob(taskJob)
-}
-
-type ScanMode string
-
-var (
-	ScanModeFast  ScanMode = "fast"
-	ScanModeSmart ScanMode = "smart"
-	ScanModeFuzz  ScanMode = "fuzz"
-)
-
-type FullScanOptions struct {
-	Title           string              `json:"title" validate:"omitempty,min=1,max=255"`
-	StartURLs       []string            `json:"start_urls" validate:"required,dive,url"`
-	MaxDepth        int                 `json:"max_depth" validate:"min=0"`
-	MaxPagesToCrawl int                 `json:"max_pages_to_crawl" validate:"min=0"`
-	ExcludePatterns []string            `json:"exclude_patterns"`
-	WorkspaceID     uint                `json:"workspace_id" validate:"required,min=0"`
-	PagesPoolSize   int                 `json:"pages_pool_size" validate:"min=1,max=100"`
-	Headers         map[string][]string `json:"headers" validate:"omitempty"`
-	InsertionPoints []string            `json:"insertion_points" validate:"omitempty,dive,oneof=param url body header cookie"`
-	Mode            ScanMode            `json:"mode" validate:"omitempty,oneof=fast smart fuzz"`
-}
-
-type HistoryItemScanOptions struct {
-	WorkspaceID     uint     `json:"workspace_id" validate:"required,min=0"`
-	TaskID          uint     `json:"task_id" validate:"required,min=0"`
-	Mode            ScanMode `json:"mode" validate:"omitempty,oneof=fast smart fuzz"`
-	InsertionPoints []string `json:"insertion_points" validate:"omitempty,dive,oneof=param url body header cookie"`
-}
-
-func (o HistoryItemScanOptions) IsScopedInsertionPoint(insertionPoint string) bool {
-	for _, ip := range o.InsertionPoints {
-		if ip == insertionPoint {
-			return true
-		}
-	}
-	return false
 }
 
 func (s *ScanEngine) FullScan(options FullScanOptions, waitCompletion bool) {
---
+++ pkg/scan/options.go
@@ -0,0 +1,38 @@
+package scan
+
+type ScanMode string
+
+var (
+	ScanModeFast  ScanMode = "fast"
+	ScanModeSmart ScanMode = "smart"
+	ScanModeFuzz  ScanMode = "fuzz"
+)
+
+type FullScanOptions struct {
+	Title           string              `json:"title" validate:"omitempty,min=1,max=255"`
+	StartURLs       []string            `json:"start_urls" validate:"required,dive,url"`
+	MaxDepth        int                 `json:"max_depth" validate:"min=0"`
+	MaxPagesToCrawl int                 `json:"max_pages_to_crawl" validate:"min=0"`
+	ExcludePatterns []string            `json:"exclude_patterns"`
+	WorkspaceID     uint                `json:"workspace_id" validate:"required,min=0"`
+	PagesPoolSize   int                 `json:"pages_pool_size" validate:"min=1,max=100"`
+	Headers         map[string][]string `json:"headers" validate:"omitempty"`
+	InsertionPoints []string            `json:"insertion_points" validate:"omitempty,dive,oneof=param urlpath body header cookie json xml"`
+	Mode            ScanMode            `json:"mode" validate:"omitempty,oneof=fast smart fuzz"`
+}
+
+type HistoryItemScanOptions struct {
+	WorkspaceID     uint     `json:"workspace_id" validate:"required,min=0"`
+	TaskID          uint     `json:"task_id" validate:"required,min=0"`
+	Mode            ScanMode `json:"mode" validate:"omitempty,oneof=fast smart fuzz"`
+	InsertionPoints []string `json:"insertion_points" validate:"omitempty,dive,oneof=param urlpath body header cookie json xml"`
+}
+
+func (o HistoryItemScanOptions) IsScopedInsertionPoint(insertionPoint string) bool {
+	for _, ip := range o.InsertionPoints {
+		if ip == insertionPoint {
+			return true
+		}
+	}
+	return false
+}

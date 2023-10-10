954a78639150cc43b05a78ed3ec1fe0beb09cc2c
pyneda <11313340+pyneda@users.noreply.github.com>
2023-10-10 20:11:19 +0200

    websocket handling changes

--- pkg/proxy/ws.go
+++ pkg/proxy/ws.go
@@ -12,6 +12,13 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
+type Client struct {
+	ws   *websocket.Conn
+	send chan *InterceptedRequest
+}
+
+var clients = make(map[*Client]bool)
+
 type WSMessage struct {
 	Action   string             `json:"action"`
 	Modified InterceptedRequest `json:"request"`
@@ -26,6 +33,31 @@ func (p *Proxy) InterceptEndpoint(w http.ResponseWriter, r *http.Request) {
 	}
 	defer ws.Close()
 
+	client := &Client{ws: ws, send: make(chan *InterceptedRequest)}
+	clients[client] = true
+	defer delete(clients, client)
+
+	go p.handleMessages(client)
+	p.forwardRequestsToClients()
+}
+
+func (p *Proxy) forwardRequestsToClients() {
+	for {
+		req := <-p.ReqChannel
+		interceptedReq, err := ConvertToInterceptedRequest(req)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to convert to InterceptedRequest")
+			continue
+		}
+		for client := range clients {
+			client.send <- interceptedReq
+		}
+	}
+}
+
+func (p *Proxy) handleMessages(client *Client) {
+	defer client.ws.Close()
+
 	for {
 		// Wait for a request from the proxy interception
 		req := <-p.ReqChannel
@@ -39,23 +71,25 @@ func (p *Proxy) InterceptEndpoint(w http.ResponseWriter, r *http.Request) {
 		}
 
 		// Send that intercepted request to the client through the WebSocket
-		err = ws.WriteJSON(interceptedReq)
+		err = client.ws.WriteJSON(interceptedReq)
 		if err != nil {
 			log.Error().Err(err).Msg("Failed to send request to WebSocket")
-			continue
+			delete(clients, client)
+			break
 		}
 		log.Info().Str("url", interceptedReq.URL).Str("method", interceptedReq.Method).Msg("Sent an intercepted request to client")
 
-		// Read the decision (and potentially a modified request) from the WebSocket client
+		// Read the decision from the WebSocket client
 		var msg WSMessage
-		err = ws.ReadJSON(&msg)
+		err = client.ws.ReadJSON(&msg)
 		if err != nil {
 			log.Error().Err(err).Msg("Failed to read message from WebSocket")
-			continue
+			delete(clients, client)
+			break
 		}
 
 		if msg.Action == "forward" && msg.Modified.URL != "" && msg.Modified.Method != "" {
-			log.Info().Str("action", msg.Action).Str("url", msg.Modified.URL).Str("method", msg.Modified.Method).Msg("Got an intercept result from client")
+			log.Warn().Str("action", msg.Action).Str("url", msg.Modified.URL).Str("method", msg.Modified.Method).Msg("Got an intercept result from client")
 			actualReq, err := ConvertFromInterceptedRequest(&msg.Modified)
 			if err != nil {
 				log.Error().Err(err).Msg("Failed to convert from InterceptedRequest")

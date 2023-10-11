6f15c426cbf17547d4a4d64c4f3debca33fb6976
pyneda <11313340+pyneda@users.noreply.github.com>
2023-10-11 22:43:27 +0200

    embed yaml payload generators into the binary and also allow to load from user specified directory

--- pkg/payloads/generation/loader.go
+++ pkg/payloads/generation/loader.go
@@ -1,6 +1,7 @@
 package generation
 
 import (
+	"embed"
 	"github.com/rs/zerolog/log"
 	"gopkg.in/yaml.v3"
 	"os"
@@ -8,14 +9,17 @@ import (
 	"strings"
 )
 
-// loadGenerator reads an individual file and map it into an instance of PayloadGenerator
+//go:embed templates/*
+var localTemplates embed.FS
+
+// loadGenerator reads an individual file and maps it into an instance of PayloadGenerator
 func loadGenerator(filePath string) (*PayloadGenerator, error) {
-	var pg PayloadGenerator
 	data, err := os.ReadFile(filePath)
 	if err != nil {
 		return nil, err
 	}
 
+	var pg PayloadGenerator
 	err = yaml.Unmarshal(data, &pg)
 	if err != nil {
 		return nil, err
@@ -24,8 +28,48 @@ func loadGenerator(filePath string) (*PayloadGenerator, error) {
 	return &pg, nil
 }
 
-// LoadGenerators handle reading all YAML files in a directory and parsing them into PayloadGenerator instances
-func LoadGenerators(dir string) ([]*PayloadGenerator, error) {
+// loadGeneratorFromFS reads an individual file from the specified FS and maps it into an instance of PayloadGenerator
+func loadGeneratorFromFS(fs embed.FS, path string) (*PayloadGenerator, error) {
+	data, err := fs.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var pg PayloadGenerator
+	err = yaml.Unmarshal(data, &pg)
+	if err != nil {
+		return nil, err
+	}
+	return &pg, nil
+}
+
+// LoadLocalGenerators loads all generators from the local directory
+func LoadLocalGenerators() ([]*PayloadGenerator, error) {
+	return loadGeneratorsFromFS(localTemplates, "templates")
+}
+
+// loadGeneratorsFromFS loads all generators from the specified FS
+func loadGeneratorsFromFS(fs embed.FS, root string) ([]*PayloadGenerator, error) {
+	entries, err := fs.ReadDir(root)
+	if err != nil {
+		return nil, err
+	}
+
+	var generators []*PayloadGenerator
+	for _, entry := range entries {
+		if !entry.IsDir() && (strings.HasSuffix(entry.Name(), ".yaml") || strings.HasSuffix(entry.Name(), ".yml")) {
+			pg, err := loadGeneratorFromFS(fs, filepath.Join(root, entry.Name()))
+			if err != nil {
+				log.Error().Err(err).Msgf("Failed to load generator %s", entry.Name())
+			} else {
+				generators = append(generators, pg)
+			}
+		}
+	}
+	return generators, nil
+}
+
+// LoadUserGenerators loads all generators from the user specified directory
+func LoadUserGenerators(dir string) ([]*PayloadGenerator, error) {
 	var generators []*PayloadGenerator
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -45,4 +89,36 @@ func LoadGenerators(dir string) ([]*PayloadGenerator, error) {
 		return nil, err
 	}
 	return generators, nil
+}
+
+// LoadGenerators loads all generators from the local and user directories
+func LoadGenerators(dir string) ([]*PayloadGenerator, error) {
+	localGenerators, err := LoadLocalGenerators()
+	if err != nil {
+		return nil, err
+	}
+	if dir == "" {
+		return localGenerators, nil
+	}
+	userGenerators, err := LoadUserGenerators(dir)
+	if err != nil {
+		return nil, err
+	}
+	return mergeGenerators(localGenerators, userGenerators), nil
+}
+
+// mergeGenerators merges local and user generators, giving priority to user generators
+func mergeGenerators(local, user []*PayloadGenerator) []*PayloadGenerator {
+	mappedGenerators := make(map[string]*PayloadGenerator)
+	for _, lg := range local {
+		mappedGenerators[lg.ID] = lg
+	}
+	for _, ug := range user {
+		mappedGenerators[ug.ID] = ug // this will replace the local one if IDs match
+	}
+	var combined []*PayloadGenerator
+	for _, v := range mappedGenerators {
+		combined = append(combined, v)
+	}
+	return combined
 }

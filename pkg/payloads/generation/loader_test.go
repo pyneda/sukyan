24faf7b052fb41d5832c715ac339ab30f9cc756c
pyneda <11313340+pyneda@users.noreply.github.com>
2023-10-11 22:43:27 +0200

    embed yaml payload generators into the binary and also allow to load from user specified directory

---
+++ pkg/payloads/generation/loader_test.go
@@ -0,0 +1,40 @@
+package generation
+
+import (
+	"testing"
+		"github.com/stretchr/testify/assert"
+
+)
+
+
+func TestMergeGenerators(t *testing.T) {
+	local := []*PayloadGenerator{
+		{ID: "1", IssueCode: "Local1"},
+		{ID: "2", IssueCode: "Local2"},
+	}
+	user := []*PayloadGenerator{
+		{ID: "2", IssueCode: "User2"},  
+		{ID: "3", IssueCode: "User3"},
+	}
+
+	result := mergeGenerators(local, user)
+	assert.NotNil(t, result)
+	assert.Equal(t, 3, len(result))
+
+	var found bool
+	for _, gen := range result {
+		if gen.ID == "2" {
+			assert.Equal(t, "User2", gen.IssueCode)
+			found = true
+		}
+	}
+	assert.True(t, found, "Overlapping generator was not found in the merged result.")
+}
+
+
+func TestLoadGenerators(t *testing.T) {
+	gens, err := LoadGenerators("")
+	assert.NoError(t, err)
+	assert.NotNil(t, gens)
+	assert.True(t, len(gens) > 0)
+}

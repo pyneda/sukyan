27ba9fef827e833f8c678b7b57dac389d23f7d04
pyneda <11313340+pyneda@users.noreply.github.com>
2023-10-21 22:09:02 +0200

    start defining types for manual fuzzing

---
+++ pkg/manual/fuzz.go
@@ -0,0 +1,30 @@
+package manual
+
+import (
+	"github.com/pyneda/sukyan/db"
+)
+
+type RequestFuzzOptions struct {
+	Raw             string                 `json:"raw" validate:"required"`
+	InsertionPoints []FuzzerInsertionPoint `json:"insertion_points" validate:"required"`
+	Session         db.PlaygroundSession   `json:"session" validate:"required"`
+	Options         RequestOptions         `json:"options"`
+}
+
+type FuzzerPayloadsGroup struct {
+	Payloads   []string `json:"payloads"`
+	Type       string   `json:"type"`
+	Processors []string `json:"processors,omitempty" validate:"omitempty,dive,oneof=base64encode base64decode urlencode urldecode sha1hash sha256hash md5hash`
+	Wordlist   string   `json:"wordlist,omitempty"`
+}
+
+type FuzzerInsertionPoint struct {
+	Start         int                   `json:"start"`
+	End           int                   `json:"end"`
+	OriginalValue string                `json:"originalValue"`
+	PayloadGroups []FuzzerPayloadsGroup `json:"payloadGroups"`
+}
+
+func Fuzz(input RequestFuzzOptions, taskID uint) error {
+	return nil
+}

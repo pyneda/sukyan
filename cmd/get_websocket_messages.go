eb560123e87ea54a7409f897444c9b07a5bc8207
pyneda <11313340+pyneda@users.noreply.github.com>
2024-04-07 04:43:44 +0200

    get ws message and connection cli commands

---
+++ cmd/get_websocket_messages.go
@@ -0,0 +1,56 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/pyneda/sukyan/db"
+	"github.com/pyneda/sukyan/lib"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+)
+
+var filterWebSocketConnectionID uint
+
+var listWebSocketMessagesCmd = &cobra.Command{
+	Use:   "messages",
+	Short: "List WebSocket messages",
+	Run: func(cmd *cobra.Command, args []string) {
+		filters := db.WebSocketMessageFilter{
+			Pagination: db.Pagination{
+				PageSize: pageSize,
+				Page:     page,
+			},
+			ConnectionID: filterWebSocketConnectionID,
+		}
+
+		messages, _, err := db.Connection.ListWebSocketMessages(filters)
+		if err != nil {
+			log.Error().Err(err).Msg("Error listing WebSocket messages")
+			return
+		}
+
+		if len(messages) == 0 {
+			fmt.Println("No WebSocket messages found")
+			return
+		}
+
+		formatType, err := lib.ParseFormatType(format)
+		if err != nil {
+			log.Error().Err(err).Msg("Error parsing format type")
+			return
+		}
+
+		formattedOutput, err := lib.FormatOutput(messages, formatType)
+		if err != nil {
+			log.Error().Err(err).Msg("Error formatting output")
+			return
+		}
+
+		fmt.Println(formattedOutput)
+	},
+}
+
+func init() {
+	getCmd.AddCommand(listWebSocketMessagesCmd)
+	listWebSocketMessagesCmd.Flags().UintVarP(&filterWebSocketConnectionID, "connection-id", "c", 0, "Filter messages by WebSocket connection ID")
+}

7e05cbc0d5df25f5f96e853b2dd636a0e4fd8e13
pyneda <11313340+pyneda@users.noreply.github.com>
2024-05-03 13:39:27 +0200

    replay request in browser

---
+++ lib/browser/utils_test.go
@@ -0,0 +1,66 @@
+package browser
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/go-rod/rod"
+)
+
+func setupMockServer() (*rod.Page, *httptest.Server) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case r.Method == "POST" && r.URL.Path == "/":
+			body, _ := io.ReadAll(r.Body)
+			// Specific condition for redirecting
+			if string(body) == "trigger bingo" {
+				http.Redirect(w, r, "/bingo", http.StatusFound)
+				return
+			} else {
+				w.Write([]byte("received a POST request"))
+			}
+		case r.Method == "GET" && r.URL.Path == "/bingo":
+			w.Write([]byte("bingo"))
+			return
+		default:
+			w.Write([]byte("received a " + r.Method + " request"))
+		}
+	}))
+
+	page := rod.New().MustConnect().MustPage()
+	return page, server
+}
+
+func TestReplayRequestInBrowser(t *testing.T) {
+	page, server := setupMockServer()
+	defer server.Close()
+
+	// Test 1: POST request that triggers redirection
+	postReq, _ := http.NewRequest("POST", server.URL, bytes.NewBufferString("trigger bingo"))
+	err := ReplayRequestInBrowser(page, postReq)
+	assert.Nil(t, err)
+	assert.Equal(t, "bingo", page.MustElement("body").MustText())
+
+	// Test 2: Normal POST request
+	normalPostReq, _ := http.NewRequest("POST", server.URL, bytes.NewBufferString("normal post"))
+	err = ReplayRequestInBrowser(page, normalPostReq)
+	assert.Nil(t, err)
+	assert.Equal(t, "received a POST request", page.MustElement("body").MustText())
+
+	// Test 3: GET request
+	getReq, _ := http.NewRequest("GET", server.URL, nil)
+	err = ReplayRequestInBrowser(page, getReq)
+	assert.Nil(t, err)
+	assert.Equal(t, "received a GET request", page.MustElement("body").MustText())
+
+	// Test 4: PUT request
+	putReq, _ := http.NewRequest(http.MethodPut, server.URL, nil)
+	err = ReplayRequestInBrowser(page, putReq)
+	assert.Nil(t, err)
+	assert.Equal(t, "received a PUT request", page.MustElement("body").MustText())
+}

2732ab06f75124fd1755eef0370a1415223b3c00
pyneda <11313340+pyneda@users.noreply.github.com>
2024-10-26 03:18:33 +0200

    remove hardcoded workspace id in some tests

--- api/tasks_test.go
+++ api/tasks_test.go
@@ -1,11 +1,13 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/pyneda/sukyan/db"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,8 +15,16 @@ func TestFindTasks(t *testing.T) {
 	app := fiber.New()
 
 	app.Get("/tasks", FindTasks)
-
-	req := httptest.NewRequest("GET", "/tasks?page=1&page_size=10&status=Completed&workspace=1", nil)
+	workspace, err := db.Connection.GetOrCreateWorkspace(&db.Workspace{
+		Title: "TestFindTasks",
+		Code:  "TestFindTasks",
+	})
+	assert.Nil(t, err)
+	req := httptest.NewRequest(
+		"GET",
+		fmt.Sprintf("/tasks?page=1&page_size=10&status=Completed&workspace=%d", workspace.ID),
+		nil,
+	)
 	resp, _ := app.Test(req)
 
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
--- api/websockets_test.go
+++ api/websockets_test.go
@@ -1,11 +1,13 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/pyneda/sukyan/db"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,18 +15,36 @@ func TestFindWebSocketConnections(t *testing.T) {
 	app := fiber.New()
 	app.Get("/api/v1/wsconnections", FindWebSocketConnections)
 
+	workspace, err := db.Connection.GetOrCreateWorkspace(&db.Workspace{
+		Title: "TestFindWebSocketConnections",
+		Code:  "TestFindWebSocketConnections",
+	})
+	assert.Nil(t, err)
+
 	// Test with valid parameters
-	req := httptest.NewRequest("GET", "/api/v1/wsconnections?page_size=2&page=1&workspace=1", nil)
+	req := httptest.NewRequest(
+		"GET",
+		fmt.Sprintf("/api/v1/wsconnections?page_size=2&page=1&workspace=%d", workspace.ID),
+		nil,
+	)
 	resp, _ := app.Test(req)
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 
 	// Test with invalid page parameter
-	req = httptest.NewRequest("GET", "/api/v1/wsconnections?page_size=2&page=abc&workspace=1", nil)
+	req = httptest.NewRequest(
+		"GET",
+		fmt.Sprintf("/api/v1/wsconnections?page_size=2&page=abc&workspace=%d", workspace.ID),
+		nil,
+	)
 	resp, _ = app.Test(req)
 	assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
 
 	// Test with invalid page_size parameter
-	req = httptest.NewRequest("GET", "/api/v1/wsconnections?page_size=xyz&page=1&workspace=1", nil)
+	req = httptest.NewRequest(
+		"GET",
+		fmt.Sprintf("/api/v1/wsconnections?page_size=xyz&page=1&workspace=%d", workspace.ID),
+		nil,
+	)
 	resp, _ = app.Test(req)
 	assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
 }
